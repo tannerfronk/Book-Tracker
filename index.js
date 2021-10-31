1387df484000ee2dac16da9d7aae63a73d9559c7
tannerfronk <tannerjace07@hotmail.com>
2021-10-31 13:08:55 -0600

    can rate books

--- index.js
+++ index.js
@@ -77,6 +77,8 @@
             addToReadList(e)
         } else if(attribute && attribute.value == 'completeBook'){
             completeBook(e)
+        } else if(attribute && attribute.value == 'rateBook'){
+            rateBook(e)
         }
     })
 
@@ -104,9 +106,7 @@
     // change read to true or false
     function completeBook(event){
         let bookID = event.target.id
-        console.log(bookID)
         let book = books.find(book => book.key === bookID)
-        console.log(book)
         if(!book.read){
             book.read = true
         } else {
@@ -117,6 +117,15 @@
         } else {
             displayCompletedList()
         }
+    }
+    
+    function rateBook(event){
+        let bookID = event.target.id
+        let ratingID = '[id="rate' + bookID + '"]'
+        let book = books.find(book => book.key === bookID)
+        let rating = document.querySelector(ratingID).value
+        book.rating = rating
+        displayCompletedList()
     }
 
     document.addEventListener('click', (e) => {
@@ -142,7 +151,8 @@
                             <div class="d-flex flex-column float-end w-25">
                                 <button id="${book.key}" buttonFunc="addToReadList" class="btn btn-success float-end mb-2">${book.readList ? 'On Read List <i class="fas fa-check"></i>' : 'Add to Read List'}</i></button>
                                 ${book.read ? 
-                                    `<button id="${book.key}" buttonFunc="completeBook" class="btn btn-success float-end">Read <i class="fas fa-check"></i></button>` :
+                                    `<button id="${book.key}" buttonFunc="completeBook" class="btn btn-success float-end">Read <i class="fas fa-check"></i></button>` 
+                                :
                                     `<button id="${book.key}" buttonFunc="completeBook" class="btn btn-success float-end">Completed the Book?</button>`
                                 }
                             </div>
@@ -162,7 +172,7 @@
     function displayCompletedList(){
         resultsArea.innerHTML = ''
         let completedLength = 0
-        books.filter((book) => {
+        books.filter((book, index) => {
             if(book.read){
                 resultsArea.innerHTML += 
                 `
@@ -171,9 +181,33 @@
                             <div class="d-flex flex-column float-end w-25">
                                 <button id="${book.key}" buttonFunc="addToReadList" class="btn btn-success float-end mb-2">${book.readList ? 'On Read List <i class="fas fa-check"></i>' : 'Add to Read List'}</button>
                                 ${book.read ? 
-                                    '<button id="${book.key}" buttonFunc="completeBook" class="btn btn-success float-end">Read <i class="fas fa-check"></i></button>' :
-                                    '<button id="${book.key}" buttonFunc="completeBook" class="btn btn-success float-end">Completed the Book?</button>'
+                                    `<button id="${book.key}" buttonFunc="completeBook" class="btn btn-success float-end mb-2">Read <i class="fas fa-check"></i></button>`
+                                :
+                                    `<button id="${book.key}" buttonFunc="completeBook" class="btn btn-success float-end mb-2">Completed the Book?</button>`
                                 }
+                                
+                                <button id="${book.key}" class="btn btn-success float-end" data-bs-toggle="modal" data-bs-target="#rateModal${index}">${book.rating ? `You Rated ${book.rating} Stars` : 'Rate Book'}</button>
+                                
+                                <div class="modal fade" id="rateModal${index}" tabindex="-1">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                        <div class="modal-body">
+                                            <p>How would you rate ${book.title} out of 5 stars?</p>
+                                            <select id="rate${book.key}">
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                            </select>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button id="${book.key}" type="button" class="btn btn-primary" buttonFunc="rateBook" data-bs-dismiss="modal">Save Rating</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <h5 class="card-title">${book.title}</h5>
                             <h6 class="card-subtitle mb-2 text-muted">${book.author ?? 'Unknown'}</h6>
